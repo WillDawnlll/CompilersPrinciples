f39be1a01d05dc01d5315d8db45eb4819bc82245
WillDawnlll <1224242080@qq.com>
2018-06-28 16:23:42 +0800

    数字，符号，处理完成，删除函数；关键字标识符停止入栈条件未考虑，空格？

--- LexicalAnalysis.py
+++ LexicalAnalysis.py
@@ -1,70 +1,66 @@
 #5类词：
 #定义关键字表:1
-KeywordList=['if','where','elesif','int','do','return']
+KeywordList = ['if','int','then','do','print']
 #定义分界符表:2
-DelimiterList=['"','{','}','(',')']
+DelimiterList = [';']
 #定义运算符表(算术运算符和关系运算符):3
-OperatorList=['=','-','+','*','/','>','<']
-#常数0,标识符5开始增，
+OperatorList = ['=','-','+','*','/','>','<']
+#常数0,标识符5，
 #单词栈
-Word=[]
+Word = []
 #输出流
-OutStream=[]
-
-def IfDelimiter():
-    pass
-
-def IfOperator(Char):
-    pass
-
-def IfNumber():
-    pass
+OutStream = []
 
 def IfKeyword():
+
     pass
-
-def RecordIdentifier():
-    SequenceNumber+=1
-    pass
-
-
 
 #取一个字符，判断属2/3否,判断0否,判断1否(根据表1构建最小DFA，入栈，),str和类型构成2元组，插入list，位置为循环计数
-SourceCodeFile=open('./SourceCode','r')
-FileEnd=0
-SequenceNumber=5
+SourceCodeFile = open('./SourceCode','r')
+FileEnd = 0
+SequenceNumber = 5
 
-while FileEnd==0:
-    Char=SourceCodeFile.read(1)
+while FileEnd == 0:
+    Char = SourceCodeFile.read(1)
     #文件结束
-    if Char=='':
-        FileEnd=1
+    if Char == '':
+        FileEnd = 1
         SourceCodeFile.close()
         break
+    if Char == (' ' or '\n'):
+        break
+
+    if Char in DelimiterList:
+        OutStream.append((Char,2))
+        break
+
+    if Char in OperatorList:
+        OutStream.append((Char,3))
+        break
+
+    #num:上一个char是数字
+
+    if num == 1 and Char.isdigit() == fause:
+        OutStream.append(Tuple(Word.pop(),0))
+        num = 0
 
     Word.append(Char)
 
-    Tuple=()
 
-    if IfDelimiter(Char)==0:
-        break
-    if IfOperator(Char)==0:
-        break
-    if IfNumber(Char)==0:
-        break
-    if IfKeyword(Char)==0:
+    if Char.isdigit() == true:
+        num = 1
         break
 
-    RecordIdentifier(Word.pop())
-
-    OutStream.append(Tuple())
+    if IfKeyword(Char) == 0:
+        OutStream.append(Tuple(Word.pop(),5))
+        break
 
 SourceCodeFile.close()
 
-OutFile=open('./OutFile','w')
+OutFile = open('./OutFile','w')
 #OutStream   list转换str  函数
 
-OutFile.write(OutStream)
+OutFile.write(str(OutStream))
 
 OutFile.close()
 
