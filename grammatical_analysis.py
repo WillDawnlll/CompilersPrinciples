c57cc14b171916e8f5b3f24a8809b2c846c46c7e
WillDawnlll <1224242080@qq.com>
2018-07-02 01:55:21 +0800

    完成LL1右递归分析函数，可以分别对每句分析，未测试，lex有错，无语法树list格式化树形输出

--- grammatical_analysis.py
+++ grammatical_analysis.py
@@ -2,59 +2,50 @@
 
 from lexical_analysis import word_stream
 
-# print(WordStream)
-#
-# for a in WordStream:
-#     print(type(a))
-
-
-# 测试 单句语法分析
-# A = 'a = a * b + c ;'
-# class SyntaxTree(object):
-#     pass
-
-# 文法类的list构建语法子树的函数（是否作为语法树类的方法？）
-
-
-# 文法类：
-# 成员为导出式
-# 方法为判断是否为此文法
-# class Grammar(object):
-#     def __init__(self,grammar):
-#         self.grammar = grammar
-#     def
-#序号
+#语法树，文法，非终结符
 class tree(list):
     def __init__(self,l):
         self.list=l
-class grammer(list):
+class grammar(list):
     def __init__(self,l):
         self.list=l
 class noterminal(list):
-    def __init__(self,g):
-        self.gra=[]
-        self.gra.append(g)
+    def __init__(self,l):
+        self.list=l
 
 
 #检测文法和句子是否匹配
-def right(t_grammer, t_sentence):
-    for seq in range(0, len(t_grammer) - 1):
-        #当前单词为非终结符,递归,检测此非终结符和剩余句子
-        if t_grammer[seq] is list:
-            #此非终结符替换为相应子句
-            t_grammer[seq] = right(t_grammer[seq], t_sentence[seq:-1])
+#只考虑右递归
+def right(t_grammar, t_sentence):
+    for seq in range(0, len(t_grammar) - 1):
+        #当前词序号对应的文法词为非终结符
+        if t_grammar[seq] is noterminal:
+            #，从此非终结符的文法表中查找
+            for tt_grammar in t_grammar[seq]:
+                #某文法首词,同当前词序号对应的句子词相同
+                if tt_grammar[0] == t_sentence[seq]:
+                    #当前词序到结尾的句子替换为相应多一层数组的return子句
+                    tt_sentence = t_sentence[seq:-1]
+                    t_sentence.pop(seq,-1)
+                    result = right(tt_grammer, tt_sentence)
+                    if result == False:
+                        return False
+                    else:
+                        return t_sentence.append(result)
         else:
-            if t_sentence[seq][1] == t_grammer[seq]:
-                if seq == len(t_grammer)-1:
+            if t_sentence[seq][1] == t_grammar[seq]:
+                if seq == len(t_grammar)-1:
                     return t_sentence
             else:
-                global num
-                print('第%s行出错\n'%num)
                 return False
 
 
+#定义文法
+E=noterminal([grammar([4,3]),grammar([5])])
+A=noterminal([grammar([4,1,E]))
+S=noterminal([grammar([1,4]),grammar([A])])
 
-
+#每句
 current_word = 0
 last_semicolon = 0
 end_word = len(word_stream)-1
@@ -72,23 +63,7 @@ while True:
             sentence = word_stream[last_semicolon:current_word]
             break
     print(sentence)
-
-    #根据首词sort选文法
-    E=noterminal([[4,3],5])
-    A=noterminal([[4,1,E]])
-    # 列表实现节点分叉不固定树
-    S=noterminal([[1,4],A])
-
-    tree = right(A,sentence)
-    
-
-
-    #调用当前文法判断函数：(多文法用一个程序？文法存在list中,0为非终结符)
-    #符合，构建语法树当前节点用此文法扩展子树
-    #不符，打印行数，输出语法错误提示,return False
-
-
-
+    tree = right(S,sentence)
     last_semicolon = current_word
 
 
