a33ddb42ac2c8224026af087042ad88cb1ca47f5
WillDawnlll <1224242080@qq.com>
2018-07-02 13:38:55 +0800

    改正right,区别noterminal和grammar

--- grammatical_analysis.py
+++ grammatical_analysis.py
@@ -11,38 +11,32 @@ class noterminal(list):
 
 #检测文法和句子是否匹配
 #只考虑右递归
-def right(t_grammar, t_sentence):
-#    print('ined right')
-#    print(t_grammar)
-#    print(len(t_grammar))
-#    print(range(0,len(t_grammar)-1))
-    for seq in range(0, len(t_grammar) - 1):
-#        print('1')
-#        print(type(t_grammar[seq]))
-        #当前词序号对应的文法词为非终结符
-        if t_grammar[seq] is noterminal:
-#            print(t_grammar[seq].name)
-            #，从此非终结符的文法表中查找
-            for tt_grammar in t_grammar[seq]:
-                #某文法首词,同当前词序号对应的句子词相同
-                if tt_grammar[0] == t_sentence[seq]:
-                    #当前词序到结尾的句子替换为相应多一层数组的return子句
-                    tt_sentence = t_sentence[seq:-1]
-                    t_sentence.pop(seq,-1)
-                    result = right(tt_grammer, tt_sentence)
-#                    print('result:   ',result,'\n')
-                    if result == False:
-                        return False
-                    else:
-
-                        return t_sentence.append(result)
-        else:
-            if t_sentence[seq][1] == t_grammar[seq]:
-                if seq == len(t_grammar)-1:
-#                    print('z:  ',t_sentence,'\n')
+def right(t_noterminal, t_sentence):
+    # 对此非终结符中每个文法
+    for tt_grammar in t_noterminal:
+        #当前文法词序号seq
+        for seq in range(0, len(tt_grammar) - 1):
+            #当前文法当前词序号是非终结符
+            if tt_grammar[seq] is noterminal:
+                #当前词序到结尾的句子和当前词序代表的终结符进入递归
+                #保存剩余句子
+                tt_sentence = t_sentence[seq:-1]
+                #当前句删除剩余句子
+                t_sentence.pop(seq,-1)
+                #检查结果,保存返回深一层子句list
+                result = right(tt_grammar[seq], tt_sentence)
+                if result == False:
+                    return False
+                else:
+                    # 当前句子后加递归返回的深一层list包裹的return子句
+                    return t_sentence.append(result)
+            #文法和句子当前词sort相符
+            elif tt_grammar[seq] == t_sentence[seq][1]:
+                #文法全部相符
+                if seq == len(t_noterminal)-1:
                     return t_sentence
-            else:
-                return False
+                else:
+                    return False
 
 
 #定义文法
@@ -59,11 +53,6 @@ current_word = 0
 last_semicolon = 0
 end_word = len(word_stream)-1
 while True:
-    print('f')
-    print('current_word',current_word)
-
-
-
     if current_word == len(word_stream)-1:
         print('分析完成')
         break
@@ -71,17 +60,12 @@ while True:
     #this_semicolon,last_semicolon中间的句子入句子栈
     while True:
         current_word += 1
-        print(word_stream[current_word][0])
 
         if word_stream[current_word][0] is ';':
             current_word += 1
             sentence = word_stream[last_semicolon:current_word]
             break
-    print('in right:\n')
-    print(list(S))
-
     tree = right(S,sentence)
-    print('tree:\n',tree)
     last_semicolon = current_word
 
 print('s-tree:\n',tree)
