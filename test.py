d1b538607bf0ad00e8b20501bb25b20381845451
WillDawnlll <1224242080@qq.com>
2018-07-02 10:43:14 +0800

    test.py中为格式化输出函数，未完成

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 # coding=utf-8
-a = '2'
+class noterminal(list):
+    def __init__(self,l):
+        self.list=l
+E=noterminal([[4,3],[5]])
+A=noterminal([[4,1,E]])
+S=noterminal([[1,4],[A]])
 
-if a =='1' or '3':
-    print(a)
+len(S)
+print(len(S))
+print(type(S))
+print('l',len(list(S)))
+print(type(S))
+
+def p(tr):
+    print(str(tr))
+    for gra in non.list:
+        for gram in  gra:
+            if gram is noterminal:
+                p(gram)
+            else:
+                print(non)
