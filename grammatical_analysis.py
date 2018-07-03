01af338a76af4959fef86bae883e0bd9fe6e9a33
WillDawnlll <1224242080@qq.com>
2018-07-03 21:31:51 +0800

    语法分析正确 输出list层次代表的语法树 最外层list为每句的树组成的森林 之后每层[]代表一次非终结符替换

--- grammatical_analysis.py
+++ grammatical_analysis.py
@@ -13,62 +13,76 @@ class noterminal(list):
 #只考虑右递归
 def right(t_noterminal, t_sentence):
     # 对此非终结符中每个文法
-    for tt_grammar in t_noterminal:
+    for production in eval(t_noterminal):
         #当前文法词序号seq
-        for seq in range(0, len(tt_grammar) - 1):
+        for sequence in range(0, len(production)):
             #当前文法当前词序号是非终结符
-            if tt_grammar[seq] is noterminal:
+            if type(production[sequence]) is str:
                 #当前词序到结尾的句子和当前词序代表的终结符进入递归
                 #保存剩余句子
-                tt_sentence = t_sentence[seq:-1]
+                t = t_sentence[sequence:]
                 #当前句删除剩余句子
-                t_sentence.pop(seq,-1)
+                t_sentence = t_sentence[:sequence]
                 #检查结果,保存返回深一层子句list
-                result = right(tt_grammar[seq], tt_sentence)
+                result = right(production[sequence], t)
                 if result == False:
-                    return False
+                    t_sentence.extend(t)
+                    break
+                    # return False
                 else:
                     # 当前句子后加递归返回的深一层list包裹的return子句
-                    return t_sentence.append(result)
-            #文法和句子当前词sort相符
-            elif tt_grammar[seq] == t_sentence[seq][1]:
-                #文法全部相符
-                if seq == len(t_noterminal)-1:
+                    t_sentence.append(result)
                     return t_sentence
-                else:
+            #文法和句子当前词sort相符
+            elif production[sequence] == t_sentence[sequence][1]:
+                #文法全部相符
+                l=len(t_noterminal)-1
+                if sequence == len(t_sentence)-1:
+                    return t_sentence
+            else:
+                if production is eval(t_noterminal)[-1]:
                     return False
+                else:
+                    break
 
 
 #定义文法
-#E=noterminal([grammar([4,3]),grammar([5])])
-#A=noterminal([grammar([4,1,E])])
-#S=noterminal([grammar([1,4]),grammar([A])])
-E=noterminal([[4,3],[5]])
-A=noterminal([[4,1,E]])
-S=noterminal([[1,4],[A]])
-
+#E=noterminal([([4,3]),grammar([5])])
+#A=noterminal([([4,1,E])])
+#S=noterminal([([1,4]),grammar([A])])
+E=noterminal([[4],[5]])
+A=noterminal([['E'],[4,3,'A']])
+S=noterminal([[1,'E'],['A']])
+tree=[]
 
 #每句
 current_word = 0
 last_semicolon = 0
 end_word = len(word_stream)-1
+grammar_forest=[]
+tree=[]
 while True:
-    if current_word == len(word_stream)-1:
+    if last_semicolon == len(word_stream):
         print('分析完成')
         break
     #循环从word_stream中取句子
     #this_semicolon,last_semicolon中间的句子入句子栈
     while True:
-        current_word += 1
-
+        print(word_stream[current_word][0])
         if word_stream[current_word][0] is ';':
-            current_word += 1
+            #删分号
+            # last_semicolon += 1
             sentence = word_stream[last_semicolon:current_word]
+            current_word += 1
             break
-    tree = right(S,sentence)
+        current_word += 1
+    tree = right('S',sentence)
+    if tree is False:
+        tree = '词句语法错误'
+    grammar_forest.append(tree)
     last_semicolon = current_word
 
-print('s-tree:\n',tree)
+print('\ntree:\n',grammar_forest)
 
 
 # print(tree)
